25354268ffba3cd3264d71e4789995c709d25c2e
goaldiggerpm <47710070+goaldiggerpm@users.noreply.github.com>
2025-02-04 00:51:39 +0530

    main page fix DevPM

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import { LoginForm } from '@/components/custom/loginForm';
-import createClient from '@/utils/supabase/server';
+// import createClient from '@/utils/supabase/server';
 
 export default async function Home() {
-  const supabase = await createClient();
-  const { data: users } = await supabase.from("users").select();
-  const { data: expenses } = await supabase.from("expenses").select();
+  // const supabase = await createClient();
+  // const { data: users } = await supabase.from("users").select();
+  // const { data: expenses } = await supabase.from("expenses").select();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <pre className='mt-40'  >{JSON.stringify(users, null, 2)}</pre>
-      <pre className='mt-40'  >{JSON.stringify(expenses, null, 2)}</pre>
+      {/* <pre className='mt-40'  >{JSON.stringify(users, null, 2)}</pre>
+      <pre className='mt-40'  >{JSON.stringify(expenses, null, 2)}</pre> */}
       <LoginForm />
     </div>
   );
