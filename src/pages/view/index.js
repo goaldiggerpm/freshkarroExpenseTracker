64a800c0b8f84fde1b80080f3f6a106bad9b5dd4
goaldiggerpm <47710070+goaldiggerpm@users.noreply.github.com>
2025-03-18 21:49:35 +0530

    view page fix Dev-PM

--- src/pages/view/index.js
+++ src/pages/view/index.js
@@ -50,7 +50,7 @@ export default function View(props) {
                 setPage(currentPage)
                 setTotalPages(total)
                 if (!username) {
-                    await handleLogout('login')
+                    // await handleLogout('login')
                 }
             }
         }
